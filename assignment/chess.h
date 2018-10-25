c081f80c2ce0c2fb56ac7ae47fff27e2d589aa69
Kyrillus Khnifes <khnifes.kyrillus@gmail.com>
2018-10-25 13:35:31 +0200

    ka

--- assignment/chess.h
+++ assignment/chess.h
@@ -10,3 +10,25 @@
  * Basic chess functions.
  * ----------------------------------------------------------
  */
+ #include <stdbool.h>
+
+ enum Color {White, Black};
+
+ enum PieceType
+ {
+   Pawn, Rook, Knight, Bishop, Queen, King
+ };
+
+ struct ChessPiece{
+     enum Color color;
+     enum PieceType type;
+ };
+
+ struct ChessSquare{
+     bool is_set;
+     struct ChessPiece piece;
+ };
+
+ typedef ChessSquare ChessBoard[8][8];
+ bool is_piece(struct ChessPiece piece, enum PieceType type, enum Color color);
+ void init_chess_board(Chessboard chess_board);
