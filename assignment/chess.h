f76a5729bfb3c4f2a9004711030155f059fbc52e
Kyrillus Khnifes <khnifes.kyrillus@gmail.com>
2018-10-26 16:15:16 +0200

    Fixed a lot of errors only 3 left

--- assignment/chess.h
+++ assignment/chess.h
@@ -25,10 +25,31 @@
  };
 
  struct ChessSquare{
-     bool is_set;
+     bool is_occupied;
      struct ChessPiece piece;
+
  };
 
+
+ typedef int Rank;
+ typedef char File;
  typedef ChessSquare ChessBoard[8][8];
- bool is_piece(struct ChessPiece piece, enum PieceType type, enum Color color);
+ typedef ChessSquare Chessboard[8][8];
  void init_chess_board(Chessboard chess_board);
+ void setup_chess_board(Chessboard chess_board);
+ bool is_square_occupied(ChessBoard chess_board,int file, int rank);
+
+ struct ChessSquare* get_square(ChessBoard chess_board,int file, int rank);
+ bool add_piece(ChessBoard chess_board, char file ,int rank,struct ChessPiece piece);
+ struct ChessPiece get_piece(ChessBoard chess_board, char file ,int rank);
+ bool remove_piece(ChessBoard chess_board, char Xpos ,int Ypos);
+ bool is_piece(struct ChessPiece piece, enum Color color ,enum PieceType type);
+
+ bool squares_share_rank(char file1, int rank1, char file2, int rank2);
+ bool squares_share_file(char file1, int rank1, char file2, int rank2);
+ bool squares_share_diagonal(char file1, int rank1, char file2, int rank2);
+
+ bool squares_share_knights_move(char file1, int rank1, char file2, int rank2);
+ bool squares_share_kings_move(char file1, int rank1, char file2, int rank2);
+ bool squares_share_pawns_move(enum Color color, NormalMove, char file1, int rank1, char file2, int rank2);
+ bool squares_share_queens_move(char file1, int rank1, char file2, int rank2);
